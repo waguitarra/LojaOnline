359dce7f50ca336468a0c8b39cf895e8ae2b8f97
waguitarra <wagner@macrosassessorias.com.br>
2020-05-29 00:52:56 +0200

    Melhorias formato das mensagens de validaçao bootstrap en ngClass(Angular)

--- LojaOnline.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ LojaOnline.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/model/usuario';
 
 @Component({
   selector: 'app-login',
@@ -6,12 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public usuario;
+  public usarioAutenticado: boolean;
 
-  public email = "leo@teste.com";
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.usuario = new Usuario();
   }
 
+  entrar() {
+    if (this.usuario.email === 'waguitarra@hotmail.com' && this.usuario.senha === '1234') {
+      this.usarioAutenticado = true;
+    } else {
+      this.usarioAutenticado = false;
+    }
+  }
+  ngOnInit() {}
 }
