34bfda7b7edf1a3aff72688e5c2a3291d496e60b
waguitarra <wagner@macrosassessorias.com.br>
2020-05-28 16:46:00 +0200

    Iniciando Template em Angular

--- LojaOnline.Web/ClientApp/src/app/app.module.ts
+++ LojaOnline.Web/ClientApp/src/app/app.module.ts
@@ -9,26 +9,40 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { LoginComponent } from './usuario/login/login.component';
+import { CadastroComponent } from './usuario/cadastro/cadastro.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+   declarations: [
+      AppComponent,
+      NavMenuComponent,
+      HomeComponent,
+      CounterComponent,
+      FetchDataComponent,
+      ProdutoComponent,
+      LoginComponent,
+      CadastroComponent
+
+   ],
+   imports: [
+    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'produto', component: ProdutoComponent},
+      { path: 'login', component: LoginComponent},
+
     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+//BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
